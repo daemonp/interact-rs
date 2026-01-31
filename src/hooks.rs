a54965e37852a7e7f02c4bbfcc8faae4df67a47a
Damon Petta <damon@petta.org>
2026-01-30 21:24:47 -0500

    Bump version to 1.2.0

--- src/hooks.rs
+++ src/hooks.rs
@@ -14,7 +14,7 @@ use std::sync::atomic::{AtomicBool, Ordering};
 // =============================================================================
 
 pub const MAJOR_VERSION: u32 = 1;
-pub const MINOR_VERSION: u32 = 0;
+pub const MINOR_VERSION: u32 = 2;
 pub const PATCH_VERSION: u32 = 0;
 
 // =============================================================================
